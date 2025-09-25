8ce0db6955cae14c51df5494b3d9cbb3de62dea8
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-09-25 11:30:57 -0500

    no-v1

--- test_docker.sh
+++ test_docker.sh
@@ -8,7 +8,6 @@ docker run \
     --md_niigz /INPUTS/dwmri_tensor_md.nii.gz \
     --rd_niigz /INPUTS/dwmri_tensor_rd.nii.gz \
     --ad_niigz /INPUTS/dwmri_tensor_ad.nii.gz \
-    --v1_niigz /INPUTS/dwmri_tensor_v1.nii.gz \
     --md_threshold 0.0003 \
     --label_info "PROJECT SUBJECT SESSION" \
     --out_dir /OUTPUTS
