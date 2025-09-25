fd3bb90e0ba25dccba90fc296666b0f107c0687e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-09-25 09:52:05 -0500

    tester

--- test_docker.sh
+++ test_docker.sh
@@ -6,6 +6,9 @@ docker run \
     baxterprogers/tbss-enigma:test \
     --fa_niigz /INPUTS/dwmri_tensor_fa.nii.gz \
     --md_niigz /INPUTS/dwmri_tensor_md.nii.gz \
+    --rd_niigz /INPUTS/dwmri_tensor_rd.nii.gz \
+    --ad_niigz /INPUTS/dwmri_tensor_ad.nii.gz \
+    --v1_niigz /INPUTS/dwmri_tensor_v1.nii.gz \
     --md_threshold 0.0003 \
     --label_info "PROJECT SUBJECT SESSION" \
     --out_dir /OUTPUTS
